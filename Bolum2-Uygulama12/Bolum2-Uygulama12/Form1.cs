1eab0239625ed2ef576d9dca0b8df33bdeb8e0f2
FatimeCorumlu <109205167+battleAngel01@users.noreply.github.com>
2023-09-29 15:08:25 +0300

    Add files via upload

---
+++ Bolum2-Uygulama12/Bolum2-Uygulama12/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bolum2_Uygulama12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value == 0)
+            {
+                label1.Text = "SES YOK";
+                label1.ForeColor = Color.Black;
+            }
+
+
+            else if (trackBar1.Value > 0 && trackBar1.Value <= 10)
+            {
+                label1.Text = "SES NORMAL";
+                label1.ForeColor = Color.Green;
+            }
+
+            else if (trackBar1.Value>10)
+            {
+                label1.Text = "SES YÜKSEK";
+                label1.ForeColor = Color.Red;
+            }
+               
+        }
+    }
+}
