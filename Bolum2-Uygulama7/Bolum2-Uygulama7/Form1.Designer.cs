bed5c3261710439401df5ef86bbe4c52d28a4c75
FatimeCorumlu <109205167+battleAngel01@users.noreply.github.com>
2023-09-29 15:01:52 +0300

    Add files via upload

---
+++ Bolum2-Uygulama7/Bolum2-Uygulama7/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Bolum2_Uygulama7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hesaplaBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.toplaRadio = new System.Windows.Forms.RadioButton();
+            this.carpRadio = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // hesaplaBtn
+            // 
+            this.hesaplaBtn.BackColor = System.Drawing.Color.RosyBrown;
+            this.hesaplaBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.hesaplaBtn.Location = new System.Drawing.Point(151, 249);
+            this.hesaplaBtn.Name = "hesaplaBtn";
+            this.hesaplaBtn.Size = new System.Drawing.Size(154, 29);
+            this.hesaplaBtn.TabIndex = 0;
+            this.hesaplaBtn.Text = "HESAPLA";
+            this.hesaplaBtn.UseVisualStyleBackColor = false;
+            this.hesaplaBtn.Click += new System.EventHandler(this.hesaplaBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.RosyBrown;
+            this.groupBox1.Controls.Add(this.carpRadio);
+            this.groupBox1.Controls.Add(this.toplaRadio);
+            this.groupBox1.Location = new System.Drawing.Point(53, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(345, 61);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İşlemler";
+            // 
+            // toplaRadio
+            // 
+            this.toplaRadio.AutoSize = true;
+            this.toplaRadio.BackColor = System.Drawing.Color.RosyBrown;
+            this.toplaRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.toplaRadio.Location = new System.Drawing.Point(54, 17);
+            this.toplaRadio.Name = "toplaRadio";
+            this.toplaRadio.Size = new System.Drawing.Size(62, 21);
+            this.toplaRadio.TabIndex = 0;
+            this.toplaRadio.TabStop = true;
+            this.toplaRadio.Text = "Topla";
+            this.toplaRadio.UseVisualStyleBackColor = false;
+            // 
+            // carpRadio
+            // 
+            this.carpRadio.AutoSize = true;
+            this.carpRadio.BackColor = System.Drawing.Color.RosyBrown;
+            this.carpRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.carpRadio.Location = new System.Drawing.Point(177, 19);
+            this.carpRadio.Name = "carpRadio";
+            this.carpRadio.Size = new System.Drawing.Size(56, 21);
+            this.carpRadio.TabIndex = 1;
+            this.carpRadio.TabStop = true;
+            this.carpRadio.Text = "Çarp";
+            this.carpRadio.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.RosyBrown;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(230, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.RosyBrown;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(230, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 30);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(100, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sayı 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(100, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sayı 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(164, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sonuç : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(461, 299);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.hesaplaBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hesaplaBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton carpRadio;
+        private System.Windows.Forms.RadioButton toplaRadio;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Bolum2-Uygulama7/Bolum2-Uygulama7/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bolum2_Uygulama7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool carp, topla;
+        float sayi1, sayi2, sonuc;
+        private void hesaplaBtn_Click(object sender, EventArgs e)
+        {
+            carp = carpRadio.Checked;
+            topla = toplaRadio.Checked;
+
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+
+            if (carp)
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            else if (topla)
+            {
+                sonuc = sayi1 + sayi2;
+            }
+
+            label3.Text = "Sonuç : " + sonuc;
+        }
+    }
+}
